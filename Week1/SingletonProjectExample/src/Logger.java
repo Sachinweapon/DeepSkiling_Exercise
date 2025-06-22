a0cbeedb80c405a054b4ac832102dc82592af29f
Sachinweapon <sachinpanigrahi03@gmail.com>
2025-06-22 06:03:35 +0530

    Creating the Test class

--- Week1/SingletonProjectExample/src/Logger.java
+++ Week1/SingletonProjectExample/src/Logger.java
@@ -5,5 +5,15 @@ public class Logger
   {
     System.out.println("Logger initialized");
   }
-
+  public static Logger getInstance() 
+  {
+    if (instance == null) {
+        instance = new Logger();
+    }
+    return instance;
+  }
+  public void log(String message) 
+  {
+    System.out.println("LOG: " + message);
+  }
 }
